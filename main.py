2d164985c55edf44b2a4df4fa351619618092fa2
ahkeop <71476103+ahkeop@users.noreply.github.com>
2020-09-18 11:12:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
-This is a sample Python script.
+import discord
+import asyncio
+import os
 
-import discod
+client = discord.Client()
 
-client = discod.client()
+token = "acess_token"
+client = discord.Client()
+@client.event
+async def on_ready():
+    print('Is any one there?')
+    print(client.user.name)
+    print(client.user.id)
+
+
 
 
 @client.event
 async def on_ready():
     print(client.user.id)
     print("ready")
-    game = discod.Game("아잉")
-    await client.change_presence(status=discod.Status.online, activity=game)
+    game = discord.Game("휴먼아 도움말/베타테스팅")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
 
 @client.event
 async def on_message(message):
-    if message.content.startwith("휴먼아"):
-        await message.channel.send("ㅎㅇ")
+    if message.content == '휴먼아 도움말':
+        embed = discord.Embed(color=0xff00, title="미먼봇 도움말", description="미먼봇은 단순 장난용으로 만들어진 챗봇입니다\n명령어 목록\n`휴먼아`\n`휴먼아 안녕`\n`휴먼아 ㅗㅗ`\n`휴먼아 와샌즈`\n`휴먼아 누나`", timestamp=message.created_at)
+        embed.set_footer(text=message.author, icon_url=message.author.avatar_url)
+        await message.channel.send(embed=embed)
 
-client.run("NzU2MDMxMjE5NjQ0NDk3OTYw.X2L68Q.DXmdDYWXs-oZZZDjlZI2gdL-bUA")
+    if message.content == '휴먼아':
+        await message.channel.send("ㅇㅇ 왜")
+    if message.content == '휴먼아 ㅗㅗ':
+        await message.channel.send('내가 만만해!?, 내가 만만하냐고!!!')
+    if message.content == '휴먼아 안녕':
+        await message.channel.send('ㅎㅇㅎㅇ')
+    if message.content == '휴먼아 누나':
+        await message.channel.send('나죽어')
+    if message.content == '휴먼아 와샌즈':
+        await message.channel.send('아시는구나!!')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+acess_token = os.environ["BOT.TOKEN"]
+client.run(acess_token)
